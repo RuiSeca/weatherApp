d0c29fd830f0ba6cca4b57db9ffbb7c77a3ddd44
RuiSeca <158776611+RuiSeca@users.noreply.github.com>
2024-02-04 23:41:00 +0000

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,13 @@
-// script.js
-
 const apiKey = '5a0a261f16b6352ff63cdb857d7a97bd';
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+document.getElementById('weatherButton').addEventListener('click', getWeather);
+
+// Initial call to get weather
+getWeather();
+
+// Set interval to update weather every 10 minutes (600,000 milliseconds)
+setInterval(getWeather, 600000);
 
 async function getWeather() {
   const location = document.getElementById('locationInput').value;
@@ -11,7 +17,30 @@ async function getWeather() {
 }
 
 function displayWeather(data) {
-  document.getElementById('location').textContent = data.name;
-  document.getElementById('temperature').textContent = `Temperature: ${data.main.temp}°C`;
-  document.getElementById('description').textContent = `Description: ${data.weather[0].description}`;
+  document.getElementById('location').textContent = `Location: ${data.name}`;
+  document.getElementById('temperature').textContent = `Temperature: ${Math.round(data.main.temp)}°C`;
+  document.getElementById('humidity').textContent = `Humidity: ${data.main.humidity}%`;
+  document.getElementById('wind').textContent = `Wind Speed: ${data.wind.speed} m/s`;
+
+  const weatherIcon = document.querySelector('.weather-icon');
+
+  switch (data.weather[0].main) {
+    case "Clouds":
+      weatherIcon.src = "assets/images/clouds.png";
+      break;
+    case "Clear":
+      weatherIcon.src = "assets/images/clear.png";
+      break;
+    case "Rain":
+      weatherIcon.src = "assets/images/rain.png";
+      break;
+    case "Drizzle":
+      weatherIcon.src = "assets/images/drizzle.png";
+      break;
+    case "Mist":
+      weatherIcon.src = "assets/images/mist.png";
+      break;
+    default:
+      weatherIcon.src = "assets/images/default.png"; // Set a default image or leave it blank
+  }
 }
